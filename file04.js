04d7d6c241e66bfa6883847cb41297f11fc8102f
anil kumar <40019042+anilkumarsingh1911@users.noreply.github.com>
2021-09-02 15:33:09 +0530

    vid 6
    https://www.youtube.com/watch?v=mjH_lzRpJa4&list=PL8p2I9GklV44X970xDCQvts19-0XQWMeA&index=6

---
+++ file04.js
@@ -0,0 +1,5 @@
+var other = require('./function')
+var add = require('./function')
+
+console.log(other());
+console.log(add(1, 2));
---
+++ function.js
@@ -0,0 +1,10 @@
+var other = function () {
+    return ('other = ');
+}
+
+var add = function (a, b) {
+    return (a + b);
+}
+
+module.exports = other;
+module.exports = add;
